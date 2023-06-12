0379b455d709fc8de18657fab297147ea0b73ec6
iriszong <77033184+IrisZong@users.noreply.github.com>
2023-06-12 14:05:32 -0400

    Add files via upload

---
+++ Final1.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.final1;
+
+/**
+ *
+ * @author zongruitong
+ */
+public class Final1 {
+
+    public static void main(String[] args) {
+       
+      
+        
+        int sum=0;
+        int n=0;
+        
+         while(n<100)  {
+             n+=1;
+             sum+=n;
+             
+         }    
+                         
+            
+            
+         System.out.println("the sum of 1 to 100 is "+sum); 
+             
+           
+    }
+}
---
+++ final2.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author zongruitong
+ */
+public class final2 {
+    
+      public static void main(String[] args) {
+          
+       
+    for(int i=0;i<5;i++){
+        
+        for(int j=0;j<8;j++){
+            
+            System.out.print("*");
+            
+            
+            
+        }
+         System.out.println("");
+    }
+    
+}
+}
